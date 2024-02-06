35c895abf70d43b427064d962dd127743317268c
xxjrtxx <xxjrtxx@pop-os.localdomain>
2024-02-05 23:54:52 -0600

    more changes

--- client/components/Main/Det_Ship_Rev.jsx
+++ client/components/Main/Det_Ship_Rev.jsx
@@ -5,50 +5,104 @@ import uparrow from "../../../images/up_arrow.png";
 import rating from "../../../images/4.8_star_rating.png";
 let normalReview = "reviews";
 let expandedReview = "expReviews";
+let expandedReview2 = "expReviews2";
+let normalShipping = "shipping";
+let expandedShipping = "expShipping";
 
 const Det_Ship_Rev = () => {
+  const [currentReviewID, setReviewID] = useState(normalReview);
+  const [currentSizeArrow, setSizeArrow] = useState(downarrow);
   const [currentShipArrow, setShipArrow] = useState(downarrow);
   const [currentRevArrow, setRevArrow] = useState(downarrow);
-  const [isParagraphVisible, setParagraphVisibility] = useState(false);
+  const [isSizeParagraphVisible, setSizeParagraphVisibility] = useState(false);
+  const [isShipParagraphVisible, setShipParagraphVisibility] = useState(false);
+  const [isReviewParagraphVisible, setReviewParagraphVisibility] =
+    useState(false);
+  const [currentShippingClass, setShippingClass] =
+    useState("downarrow-shipping");
+
+  const sizeChangeArrow = () => {
+    setSizeArrow((prevArrow) =>
+      prevArrow === downarrow ? uparrow : downarrow
+    );
+    setShippingClass((prevClass) =>
+      prevClass === "downarrow-shipping"
+        ? "expDownarrow-shipping"
+        : "downarrow-shipping"
+    );
+
+    setReviewID((prevID) =>
+      prevID === normalReview ? expandedReview2 : normalReview
+    );
+
+    setSizeParagraphVisibility((prevVisibility) => !prevVisibility);
+  };
 
   const shipChangeArrow = () => {
     setShipArrow((prevArrow) =>
       prevArrow === downarrow ? uparrow : downarrow
     );
-    setParagraphVisibility((prevVisibility) => !prevVisibility);
+    setShipParagraphVisibility((prevVisibility) => !prevVisibility);
+    setReviewID((prevID) =>
+      prevID === normalReview ? expandedReview : normalReview
+    );
   };
   const reviewChangeArrow = () => {
     setRevArrow((prevArrow) => (prevArrow === downarrow ? uparrow : downarrow));
+    setReviewParagraphVisibility((prevVisibility) => !prevVisibility);
   };
   return (
     <>
       <div id="det_ship_rev">
-        <div id="details">
-          <p>View Product Details</p>
+        <div id="size_and_fit">
+          <p>Size & Fit</p>
+          <div>
+            <img
+              onClick={sizeChangeArrow}
+              className="downarrow-size"
+              src={`${currentSizeArrow}`}
+              alt=""
+            />
+            {isSizeParagraphVisible && (
+              <div className="sizePar">
+                <ul>
+                  <li>
+                    Fits large; we recommend ordering a half size<br></br>down
+                  </li>
+                  <li>
+                    <b>Size Guide</b>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
-        <div id="shipping">
-          <p>Shipping & Returns</p>
-          <p>
+
+        <div id={!isSizeParagraphVisible ? normalShipping : expandedShipping}>
+          <div>Shipping & Returns</div>
+          <div>
             <img
               onClick={shipChangeArrow}
-              className="downarrow-shipping"
+              className={currentShippingClass}
               src={`${currentShipArrow}`}
               alt=""
             />
-            {isParagraphVisible && (
-              <p className="shipPar">
+            {isShipParagraphVisible && (
+              <div className="shipPar">
                 Free standard shipping on orders $50+ and free<br></br>
-                60-day returnsfor Nike Members. Learn more.<br></br>
+                60-day returnsfor Nike Members. <span>Learn more.</span>
+                <br></br>
                 <span>Return policy exclusions apply.</span>
                 <br></br>
                 <br></br>
                 <span>Pick-up available at select Nike stores.</span>
-              </p>
+              </div>
             )}
-          </p>
+          </div>
         </div>
-        <div id={!isParagraphVisible ? normalReview : expandedReview}>
-          <p>Reviews (173)</p>
+
+        <div id={currentReviewID}>
+          <div>Reviews (173)</div>
           <p>
             <img className="star" src={`${rating}`} alt="star" />
           </p>
