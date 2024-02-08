bf94056a436667b9445f287175598a995f28cb58
stephx202 <stephhernandez202@gmail.com>
2024-02-08 13:41:52 -0800

    more css stuff added

--- client/components/Carousels/ModalBody1.jsx
+++ client/components/Carousels/ModalBody1.jsx
@@ -30,7 +30,11 @@ const ModalBody1 = (props) => {
               </ul>
           </div>
           <button className="close-btn" onClick={closeModal}>
-            Close
+          <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none">
+            <circle cx="12" cy="12" r="9" fill="#bfbfbf" fill-opacity="0.24"></circle>
+            <path d="M16 8L8 16" stroke="#222222" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M8 8L16 16" stroke="#222222" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
           </button>
         </div>
       </div>
